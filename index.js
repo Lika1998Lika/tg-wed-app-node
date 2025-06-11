cf56a1dc1a932ea629e74cb56885dbe6414e84ab
Anzhelika <anzhelika_vanuyto@mail.ru>
2025-06-11 19:11:23 +0500

    fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const webAppUrl = 'https://funny-manatee-929c25.netlify.app';
 let bot;
 
 if (!bot) {
-  bot = new TelegramBot(token, {polling: false});
+  bot = new TelegramBot(token, {polling: true});
 }
 
 const app = express();
@@ -76,5 +76,5 @@ app.post('/web-data', async (req, res) => {
   }
 })
 
-const PORT = process.env.PORT || 10000
+const PORT = 8000
 app.listen(PORT, () => console.log('start server ' + PORT))
